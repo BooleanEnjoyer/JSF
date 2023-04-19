6f72356f9be3434ee69457e5f1b9fe1453a2c90a
SuperTomek <131263273+SuperTomek@users.noreply.github.com>
2023-04-19 14:54:04 +0200

    Update PickersToOrderEnroller.java

--- src/main/java/org/example/PickersToOrderEnroller.java
+++ src/main/java/org/example/PickersToOrderEnroller.java
@@ -7,38 +7,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PickersToOrderEnroller {
-    public boolean thereIsNoTimeToPickOrder(Orders order , LocalTime timeToPick){
-        Duration durationToPickNextOrder = Duration.between(timeToPick ,
-                order.getCompleteBy().minus(order.getPickingTime()));
+    //Czemu tu wszystko jest public?
+    
+    //nazwa metody to czasownik, popraw wszędzie, staraj się to zrobić bez negacji w nazwie 
+    //stosuj entery przed metodą
+    //W intelij jest skrót CTRL+ALT+L do formatowania, użyj dla każdej klasy
+    public boolean isNoTimeToPickOrder(Orders order , LocalTime timeToPick){
+        var  = order.getCompleteBy().minus(order.getPickingTime())
+        Duration durationToPickNextOrder =             Duration     .between(timeToPick,zmienna));
         return durationToPickNextOrder.isNegative();
     }
-    public boolean orderIsTaken(List<String> enrolledOrders , Orders order){
-        for (String completedOrder : enrolledOrders) {
-            if (completedOrder.contains(order.getOrderId() + " ")) {
-                return true;
-            }
-        }
-        return false;
+    
+    
+    public boolean isOrderTaken(List<String> enrolledOrders , Orders order){
+        return enrolledOrders.anyMatch((order)->completedOrder.contains(order.getOrderId() + " ")
     }
+                                       
     public boolean previousPickingTimeIsLessThanOrderStartTime(Orders order , List<Orders> ordersList, int currentOrderIndex){
-        Duration timeLookup;
-        timeLookup = Duration.between(order.getCompleteBy().minus(order.getPickingTime()),
-                ordersList.get(currentOrderIndex).getCompleteBy());
-        if(timeLookup.isNegative() || timeLookup.isZero()){
-            return true;
-        }
-        return false;
+        zmienna1 = order.getCompleteBy().minus(order.getPickingTime());
+        zmeinna2 =ordersList.get(currentOrderIndex).getCompleteBy());
+        Duration timeLookup = Duration.between(zmienna1,zmeinna2     );
+        return (timeLookup.isNegative() || timeLookup.isZero())
     }
+                                       
     public boolean currentValueIsEqualOrGreater(BigDecimal currentOrderValue, BigDecimal higherOrderValue){
-        if(currentOrderValue.compareTo(higherOrderValue) > 0){
-            return true;
-        }
-        return false;
+        return (currentOrderValue.compareTo(higherOrderValue) > 0)
     }
-    public Orders findHigherValueOrder(List<Orders> orders , int currentOrderIndex
-            , LocalTime timeToPick , List<String> enrolledOrders){
+                                       
+    public Orders findHigherValueOrder(
+        List<Orders> orders, 
+        int currentOrderIndex, 
+        LocalTime timeToPick , 
+        List<String> enrolledOrders
+    ){
         BigDecimal higherOrderValue;
         BigDecimal currentOrderValue = orders.get(currentOrderIndex).getOrderValue();
+        //to nie jest czytlene
+        //spróbuj pozbyć się magii z indexami ( użyj zamiast tego streamów )
+        //continue i break na ogół to zło 
+        //ten warunek z ifa do metody i nazwij ładnie: 'shouldPick....'
         for (int higherValueIndex = currentOrderIndex + 1; higherValueIndex < orders.size(); higherValueIndex++) {
             higherOrderValue = orders.get(higherValueIndex).getOrderValue();
             if (currentValueIsEqualOrGreater(currentOrderValue , higherOrderValue)
@@ -52,32 +59,42 @@ public class PickersToOrderEnroller {
         }
         return orders.get(currentOrderIndex);
     }
+                                       
     public LocalTime shiftTimeToNextPick(LocalTime timeToPick, Orders order){
         return timeToPick.plus(order.getPickingTime());
     }
+                                       
     public LocalTime resetTimeToPickingStart(Store store){
         return store.getPickingStartTime();
     }
-    public List<String> pickerToOrderEnroll(Store store, List<Orders> orders){
+   
+                                       
+    public List<ScheduledOrder> pickerToOrderEnroll(Store store, List<Orders> orders){
         LocalTime timeToPick=store.getPickingStartTime();
         List<String> enrolledOrders = new ArrayList<>();
         List<String> pickerList = store.getPickers();
         int currentOrderIndex=0;
+        //wywal continue i indexy
         for(String picker : pickerList) {
             for(Orders order : orders) {
-                if (thereIsNoTimeToPickOrder(order , timeToPick)
-                        || orderIsTaken(enrolledOrders, order)){
-                    currentOrderIndex++;
-                    continue;
-                }
-                order = findHigherValueOrder(orders, currentOrderIndex , timeToPick , enrolledOrders);
-                enrolledOrders.add(picker +" "+ order.getOrderId() +" "+ timeToPick);
+                //tak mniej więcej to powinno wyglądać 
+                if (shouldPickOrderBlabla(order, picker)
+                    {
+                order = findHigherValueOrder(orders, order , timeToPick , enrolledOrders);
+                enrolledOrders.add(new SchedulerOdrer(picker, order , timeToPick);
                 timeToPick = shiftTimeToNextPick(timeToPick, order);
-                currentOrderIndex++;
+                                   }                
             }
-            currentOrderIndex=0;
+            currentOrder=null;
             timeToPick = resetTimeToPickingStart(store);
         }
         return enrolledOrders;
     }
 }
+                                       
+ //zrób klasę 
+  class ScheduledOrder{
+      Order order;
+      Picker picker;    
+      LocalTime timeToPick;
+  }                                     
